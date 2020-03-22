d60aad11b03ce6ba0dbe4bb109d44790a5d09978
jdburnham <jburnham@stu.southtexascollege.edu>
2020-03-22 16:40:45 -0500

    Movies API Done

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,27 +1,19 @@
 <template>
   <v-card>
-    <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" />
-    <v-card-title>Movie Name</v-card-title>
+    <v-img class="white--text align-end" contain :src="`${imageURL}${movie.poster_path}`" />
+    <v-card-title>{{ movie.title }}</v-card-title>
     <v-card-subtitle class="pb-0">
-      <v-chip class="ma-2" color="green darken-3">
-        Primary
-      </v-chip></v-card-subtitle
-    >
+      <v-chip v-for="genre in movie.genre_ids" :key="genre" class="ma-2 green--text text--darken-3" color="green lighten-4" x-small>
+        {{ genres.find(g => g.id === genre).name }}
+      </v-chip>
+    </v-card-subtitle>
 
-    <v-card-text class="text--primary">
-      <div>Whitehaven Beach</div>
-
-      <div>Whitsunday Island, Whitsunday Islands</div>
-    </v-card-text>
+    <v-card-text class="text--primary">{{ movie.overview }}</v-card-text>
 
     <v-card-actions>
-      <v-btn color="orange" text>
-        Share
-      </v-btn>
-
-      <v-btn color="orange" text>
-        Explore
-      </v-btn>
+      <v-btn color="red darken-3" text>Love It</v-btn>
+      <v-spacer />
+      <v-btn color="orange" text>Watchlist</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -29,5 +21,29 @@
 <script>
 export default {
   name: 'MovieCard',
+  props: {
+    movie: {
+      type: Object,
+      default: () => {},
+    },
+    imageURL: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      genres: [],
+    }
+  },
+  created() {
+    this.getGenres()
+  },
+  methods: {
+    async getGenres() {
+      const genres = await this.$axios('genre/movie/list')
+      this.genres = genres.data?.genres
+    },
+  },
 }
 </script>
